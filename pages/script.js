1194e75eafa4bb38d9f7f9f4c6d0aa7f77ec92b0
AndreJoas <andrejoas93@gmail.com>
2023-04-12 16:09:45 -0300

    appMusicSong

---
+++ pages/script.js
@@ -0,0 +1,291 @@
+// var x = document.getElementById("corpo")
+// x.addEventListener("click", function musica(){
+//     const a = document.getElementById("musica")
+//     a.play();
+// })
+var meuSom = document.getElementById("song1");
+var tocar = false;
+
+function Song() {
+    tocar ? meuSom.pause() : meuSom.play();
+};
+
+meuSom.onplaying = function() {
+    tocar = true;
+};
+meuSom.onpause = function() {
+    tocar = false;
+};
+   
+// -------------------------
+
+
+var meuSom2 = document.getElementById("song2");
+var tocar2 = false;
+
+function Song2() {
+    tocar2 ? meuSom2.pause() : meuSom2.play();
+};
+
+meuSom2.onplaying = function() {
+    tocar2 = true;
+};
+meuSom2.onpause = function() {
+    tocar2 = false;
+};
+
+// -----------------------
+
+
+var meuSom3 = document.getElementById("song3");
+var tocar3 = false;
+
+function Song3() {
+    tocar3 ? meuSom3.pause() : meuSom3.play();
+};
+
+meuSom3.onplaying = function() {
+    tocar3 = true;
+};
+meuSom3.onpause = function() {
+    tocar3 = false;
+};
+
+// ---------------------------------------------
+
+var meuSom4 = document.getElementById("song4");
+var tocar4 = false;
+
+function Song4() {
+    tocar4 ? meuSom4.pause() : meuSom4.play();
+};
+
+meuSom4.onplaying = function() {
+    tocar4 = true;
+};
+meuSom4.onpause = function() {
+    tocar4 = false;
+};
+
+
+// ---------------------------------------------
+
+var meuSom5 = document.getElementById("song5");
+var tocar5 = false;
+
+function Song5() {
+    tocar5 ? meuSom5.pause() : meuSom5.play();
+};
+
+meuSom5.onplaying = function() {
+    tocar5 = true;
+};
+meuSom5.onpause = function() {
+    tocar5 = false;
+};
+
+
+// ---------------------------------------------
+
+var meuSom6 = document.getElementById("song6");
+var tocar6 = false;
+
+function Song6() {
+    tocar6 ? meuSom6.pause() : meuSom6.play();
+};
+
+meuSom6.onplaying = function() {
+    tocar6 = true;
+};
+meuSom6.onpause = function() {
+    tocar6 = false;
+};
+
+// ---------------------------------------------
+var meuSom7 = document.getElementById("song7");
+var tocar7 = false;
+
+function Song7() {
+    tocar7 ? meuSom7.pause() : meuSom7.play();
+};
+
+meuSom7.onplaying = function() {
+    tocar7 = true;
+};
+meuSom7.onpause = function() {
+    tocar7 = false;
+};
+
+// ---------------------------------------------
+var meuSom8 = document.getElementById("song8");
+var tocar8 = false;
+
+function Song8() {
+    tocar8 ? meuSom8.pause() : meuSom8.play();
+};
+
+meuSom8.onplaying = function() {
+    tocar8 = true;
+};
+meuSom8.onpause = function() {
+    tocar8 = false;
+};
+// ---------------------------------------------
+
+var meuSom9 = document.getElementById("song9");
+var tocar9 = false;
+
+function Song9() {
+    tocar9 ? meuSom9.pause() : meuSom9.play();
+};
+
+meuSom9.onplaying = function() {
+    tocar9 = true;
+};
+meuSom9.onpause = function() {
+    tocar9 = false;
+};
+// ---------------------------------------------
+
+var meuSom10 = document.getElementById("song10");
+var tocar10 = false;
+
+function Song10() {
+    tocar10 ? meuSom10.pause() : meuSom10.play();
+};
+
+meuSom10.onplaying = function() {
+    tocar10 = true;
+};
+meuSom10.onpause = function() {
+    tocar10 = false;
+};
+
+// ---------------------------------------------
+var meuSom11 = document.getElementById("song11");
+var tocar11 = false;
+
+function Song11() {
+    tocar11 ? meuSom11.pause() : meuSom11.play();
+};
+
+meuSom11.onplaying = function() {
+    tocar11 = true;
+};
+meuSom11.onpause = function() {
+    tocar11 = false;
+};
+// ---------------------------------------------
+var meuSom12 = document.getElementById("song12");
+var tocar12 = false;
+
+function Song12() {
+    tocar12 ? meuSom12.pause() : meuSom12.play();
+};
+
+meuSom12.onplaying = function() {
+    tocar12 = true;
+};
+meuSom12.onpause = function() {
+    tocar12 = false;
+};
+// ---------------------------------------------
+var meuSom13 = document.getElementById("song13");
+var tocar13 = false;
+
+function Song13() {
+    tocar13 ? meuSom13.pause() : meuSom13.play();
+};
+
+meuSom13.onplaying = function() {
+    tocar13 = true;
+};
+meuSom13.onpause = function() {
+    tocar13 = false;
+};
+// ---------------------------------------------
+
+var meuSom14 = document.getElementById("song14");
+var tocar14 = false;
+
+function Song14() {
+    tocar14 ? meuSom14.pause() : meuSom14.play();
+};
+
+meuSom14.onplaying = function() {
+    tocar14 = true;
+};
+meuSom14.onpause = function() {
+    tocar14 = false;
+};
+// ---------------------------------------------
+var meuSom15 = document.getElementById("song15");
+var tocar15 = false;
+
+function Song15() {
+    tocar15 ? meuSom15.pause() : meuSom15.play();
+};
+
+meuSom15.onplaying = function() {
+    tocar15 = true;
+};
+meuSom15.onpause = function() {
+    tocar15 = false;
+};
+
+// ---------------------------------------------
+
+var meuSom16 = document.getElementById("song16");
+var tocar16 = false;
+
+function Song16() {
+    tocar16 ? meuSom16.pause() : meuSom16.play();
+};
+
+meuSom16.onplaying = function() {
+    tocar16 = true;
+};
+meuSom16.onpause = function() {
+    tocar16 = false;
+};
+
+
+
+// ---------------------------------------------
+
+
+
+var meuSom17 = document.getElementById("song17");
+var tocar17 = false;
+
+function Song17() {
+    tocar17 ? meuSom17.pause() : meuSom17.play();
+};
+
+meuSom17.onplaying = function() {
+    tocar17 = true;
+};
+meuSom17.onpause = function() {
+    tocar17 = false;
+};
+
+
+// ---------------------------------------------
+
+var meuSom18 = document.getElementById("song18");
+var tocar18 = false;
+
+function Song18() {
+    tocar18 ? meuSom18.pause() : meuSom18.play();
+};
+
+meuSom18.onplaying = function() {
+    tocar18 = true;
+};
+meuSom18.onpause = function() {
+    tocar18 = false;
+};
+
+// ---------------------------------------------
+
+
